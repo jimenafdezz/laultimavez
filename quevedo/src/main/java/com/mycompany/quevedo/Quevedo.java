2eadce82fb39c07652617f0b0acf6b7a2a0a46a7
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-02-23 18:00:53 +0100

    el mejor del mundo

---
+++ quevedo/src/main/java/com/mycompany/quevedo/Quevedo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quevedo;
+
+/**
+ *
+ * @author DAW107
+ */
+public class Quevedo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
