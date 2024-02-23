95cffaab8de781b16db73331cde511fcf9628c43
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-02-23 18:04:34 +0100

    el mejor del mundo

--- quevedo/src/main/java/com/mycompany/quevedo/Quevedo.java
+++ quevedo/src/main/java/com/mycompany/quevedo/Quevedo.java
@@ -12,5 +12,7 @@ public class Quevedo {
 
     public static void main(String[] args) {
         System.out.println("me cago en la leche merche");
+         System.out.println("me cago en la leche merchecita");
+        
     }
 }
